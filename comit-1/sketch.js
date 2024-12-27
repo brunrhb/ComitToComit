8af990969d6caf28d2411382edba9881c8a8781a
brunrhb <77405180+brunrhb@users.noreply.github.com>
2024-12-27 15:18:17 +0100

    ok

---
+++ comit-1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  function draw() {
+    background(50);
+    noStroke();
+    for (let i = 0; i < 5; i++) {
+      fill(frameCount % 255);
+      ellipse(
+        200+(sin(frameCount/(i+10))*(i+20)),
+        100+(i*50),
+        40,
+        40);
+    }
+  }
