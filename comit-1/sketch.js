47024c9cbdb6452ea5f6191e22d20764b08773da
brunrhb <77405180+brunrhb@users.noreply.github.com>
2024-12-28 00:09:56 +0100

    ok

--- comit-1/sketch.js
+++ comit-1/sketch.js
@@ -5,7 +5,7 @@ function setup() {
     background(50);
     noStroke();
     for (let i = 0; i < 5; i++) {
-      fill(frameCount % 255);
+      fill(255);
       ellipse(
         200+(sin(frameCount/(i+10))*(i+20)),
         100+(i*50),
