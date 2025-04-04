c8b506e62102d5b78d64ef5a20a04f98a5130141
brunrhb <77405180+brunrhb@users.noreply.github.com>
2025-04-04 23:54:26 +0200

    Update text.unit-maker.js

--- comit-11/text.unit-maker.js
+++ comit-11/text.unit-maker.js
@@ -1,5 +1,7 @@
 // Importe le texte brut comme une string JS depuis text.js
-import texte from './text.js';
+//import texte from './text.js';
+import texte from './text.js?v=1.0';
+
 
 // Fonction pour découper le texte en unités (mots)
 function loadText() {
